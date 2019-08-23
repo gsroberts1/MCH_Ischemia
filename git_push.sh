35cb2eced384960d0d8c687a6ad56c7fb6e5c561
gsroberts1 <gsroberts@wisc.edu>
2019-08-22 21:46:50 -0500

    Thu, Aug 22, 2019  9:46:50 PM

--- git_push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# GIT PUSH
-# Adds files to git staging area, commits files (commits labelled by date and time), and pushes to remote repository.git
-cd ~/Documents/MCH\ Ischemia/
-git add .
-git status
-git commit -m "$(date)"
-git push -u origin master
